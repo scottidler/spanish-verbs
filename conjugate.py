96524190a64e038206b50d299612e3ce5c380b97
Scott A. Idler <scott.a.idler@gmail.com>
2023-12-15 12:57:42 -0800

    cleaned up conjugate script

--- conjugate.py
+++ conjugate.py
@@ -88,54 +88,24 @@ def conjugate(verb, file_path, prompt, output_dir, schema_yaml):
     except Exception as e:
         sys.stderr.write(f'Error processing "{verb}": {e}\n')
 
-def get_verb_meaning(verb):
-    try:
-        prompt = (
-            f'Provide the English meaning (including "to") of the Spanish verb "{verb}" as a concise definition. '
-            'If there are multiple meanings, separate them with commas. Provide only the definitions, nothing more.'
-            'hablar: to speak, to talk'
-            'comer: to eat'
-            'vivir: to live'
-        )
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=[
-                {'role': 'system', 'content': 'You are a helpful assistant.'},
-                {'role': 'user', 'content': prompt}
-            ]
-        )
-        return response.choices[0].message['content'].strip()
-    except Exception as e:
-        logger.error(f'Error fetching meaning for "{verb}": {e}')
-        return None
-
-def add_meaning_to_file(verb, output_dir):
-    file_path = os.path.join(output_dir, f'{verb}.yml')
-    if not os.path.exists(file_path):
-        logger.error(f'File for "{verb}" not found: {file_path}')
-        return
-
-    meaning = get_verb_meaning(verb)
-    if not meaning:
-        logger.error(f'Could not find meaning for "{verb}"')
-        return
-
-    with open(file_path, 'r') as file:
-        content = file.readlines()
-
-    content.insert(0, f'meaning: {meaning}\n')
-
-    with open(file_path, 'w') as file:
-        file.writelines(content)
-    logger.info(f'Added meaning to "{verb}" -> {file_path}')
-
 def main(args):
-    parser = argparse.ArgumentParser(description='get verb conjugations using ChatGPT API.')
-    parser.add_argument('verbs', nargs='+', help='list of infinitive verbs')
-    parser.add_argument('-o', '--output', default='verbs', help='output directory (default: verbs)')
-    parser.add_argument('-p', '--prompt-only', action='store_true', help='print prompt only and exit')
-    parser.add_argument('-a', '--add-meaning', action='store_true', help='toggle to add meaning to the verb file')
-    parser.add_argument('-f', '--force', action='store_true', help='force re-conjugation and file rewrite')
+    parser = argparse.ArgumentParser(description='get verb conjugations using ChatGPT API')
+    parser.add_argument(
+        'verbs',
+        nargs='+',
+        help='list of infinitive verbs')
+    parser.add_argument(
+        '-o', '--output',
+        default='verbs',
+        help='output directory (default: verbs)')
+    parser.add_argument(
+        '-p', '--prompt-only',
+        action='store_true',
+        help='print prompt only and exit')
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='force re-conjugation and file rewrite')
     ns = parser.parse_args(args)
 
     if OPENAI_API_KEY:
@@ -166,25 +136,31 @@ def main(args):
         prefix = f'{index}/{count} ({percentage}%)'
         file_path = os.path.join(output_dir, f'{verb}.yml')
 
-        prompt = (
-            f'We are building a Spanish verb conjugation yaml file for "{verb}".'
-            'The first step is definiting the infinitive of the verb'
-            'Below are three examples of the meaning for: hablar, comer and vivir; note the "to" in each'
-            'meaning: to speak, to talk'
-            'meaning: to eat'
-            'meaning: to live'
-            'Then we definine the infinitivo and conjugate the gerundio, participio-pasado'
-            'Finally we conjugate the verb in all 14 tenses and moods\n\n'
-            f'Follow this example:\n{example_yaml}\n\n'
-            f'Validation will use this schema:\n{schema_content}\n\n'
-            'Your response should be in YAML format and conform to the field names, order '
-            'and structure specified in the schema above.'
-        )
+        prompt = f'''
+We are building a Spanish verb conjugation yaml file for "{verb}".
+The first step is definiting the infinitive of the verb. Below are
+three examples of the meaning for: hablar, comer and vivir, respectively:
+
+meaning: to speak, to talk
+meaning: to eat
+meaning: to live
+
+Note: the "to" in each meaning; don't include quotes or punctuation besides the comma separating the meanings
+
+Then we definine the infinitivo, gerundio and participio-pasado.
+Finally. we conjugate the verb in all 14 tenses and moods.
+
+Follow this example:
+{example_yaml}
+
+Validation will use this schema:
+{schema_content}
+Your response should be in YAML format and conform to the field names, order
+and structure specified in the schema above.
+'''
 
         if ns.prompt_only:
             print(prompt)
-        elif ns.add_meaning:
-            add_meaning_to_file(verb, output_dir)
         else:
             if not ns.force and os.path.exists(file_path):
                 logger.info(f'Existing "{verb}": {file_path}')
