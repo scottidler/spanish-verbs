05b9b09e63e12c3a69f527600217e5fb885451d2
Scott A. Idler <scott.a.idler@gmail.com>
2025-09-30 08:16:11 -0700

    fixed up build.rs --version to scaffold standard

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde_yaml;
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version = env!("GIT_DESCRIBE"), about, long_about = None)]
 struct Cli {
     verb: String,
     #[clap(subcommand)]
